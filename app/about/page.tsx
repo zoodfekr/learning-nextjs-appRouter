3006405d19d01d86997f684f98631bd44a6061ba
ramin <zoodfekr.r@gmail.com>
2025-06-10 21:37:42 -0700

    cleared app

--- app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Aboutus = () => {
-    return (
-        <div className='text-red-500'>Aboutus</div>
-    )
-}
-
-export default Aboutus
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import HomePage from "@/components/home/HomePage";
 import React from "react";
 
 export default function Login() {
   return (
 
-    <HomePage />
+    <>
 
 
+    </>
   );
 }
