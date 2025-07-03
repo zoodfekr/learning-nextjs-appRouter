3f0221b48e8790c08d5137c85d5dd0fb86d22977
zoodfekr <zoodfekr.r@gmail.com>
2025-07-04 03:20:32 +0330

    ssg for user page

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -9,25 +9,29 @@ const getUserData_service = async (userId: string): Promise<UserType> => {
     return response.json();
 };
 
-// export async function generateStaticParams() {
-//     const response = await fetch(`${serverUrl}/users`);
-//     const users = await response.json();
-//     const userids = users
-//         .filter((user: UserType) => user.id !== undefined && user.id !== null)
-//         .map((user: UserType) => ({ userId: user.id!.toString() }));
-//     return userids
-// }
+export async function generateStaticParams() {
+    const response = await fetch(`${serverUrl}/users`);
+    const users = await response.json();
+    const userids = users
+        .filter((user: UserType) => user.id !== undefined && user.id !== null)
+        .map((user: UserType) => ({ userId: user.id!.toString() }));
+    return userids
+}
 
 
 const Page = async ({ params }: { params: Promise<{ userId: string }> }) => {
 
     const { userId } = await params;
-
     const user = await getUserData_service(userId.toString());
 
-    console.log('>>', user);
+    return (
+        <div className="max-w-md mx-auto mt-10 p-6 bg-white shadow-lg rounded-xl text-center space-y-4">
+            <div className="text-3xl font-bold text-indigo-600">{user.username}</div>
+            <div className="text-gray-600 text-lg">📧 {user.email}</div>
+            <div className="text-sm text-gray-400">User ID: {user.id}</div>
+        </div>
 
-    return <div>Page: {user.username}</div>;
+    )
 };
 
 export default Page
